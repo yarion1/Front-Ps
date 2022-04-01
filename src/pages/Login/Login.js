8cd28382239e57e1080bbe2f2844ca4909ca0a4d
Luana Lorena de Matos Tavares <71276304+Luanatvrs@users.noreply.github.com>
2022-04-01 14:49:04 -0300

    Atualizando Login

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,13 +1,12 @@
 import React, {useState} from 'react';
 import './Login.css';
-import curvatopTop from './images/curvatop.png'
-import curvaBottom from './images/curvabottom.png'
-import retangulobtn from './images/retangulobtn.png'
+import curvatopTop from '../../assets/img/curvatop.png';
+import curvaBottom from '../../assets/img/curvabottom.png';
 import {Form, FormGroup,Input,Label,Button} from 'reactstrap';
 import {AiOutlineMail} from 'react-icons/ai';
 import {RiLockPasswordFill} from 'react-icons/ri';
 
-import { useNavigate} from 'react-router-dom'
+import { Navigate, useNavigate} from 'react-router-dom'
 
 /**
  * 
@@ -56,7 +55,11 @@ function SendLogueo(Email,Password){
   
     const [Email, setEmail] = useState("");
     const [Password, setPassword] = useState("");
+    let navigate = useNavigate();
 
+    function handleClick(){
+        navigate('/register');
+    }
  
         return(
             <>
@@ -64,48 +67,50 @@ function SendLogueo(Email,Password){
                  <div className='botaoLogin'><button className='buttonLogin'>Login</button></div>
                  <div className='curvaButtontop'><img src={curvatopTop} /></div>
                  <div className='curvaButtonbottom'><img src={curvaBottom}></img></div>      
-                 <div className='botaoSign'><button className='buttonSign' >Sign in</button></div>
+                 <div className='botaoSign'> <button className='buttonSign' onClick={handleClick}>Sign in</button> </div>
              </div>
              <div className='divright'>
 
-             <div className='iconemail'><AiOutlineMail/></div>
-             <div className='iconpassword'><RiLockPasswordFill/></div>
-             <h2 className='titleLog'>Login</h2>
-             <Form inline className='formlogin' onSubmit={(e)=>{e.preventDefault();  SendLogueo(Email,Password)}} >
-        <FormGroup floating >
-        <Input
-            placeholder="Email"
-            type="Email"
-            onChange={({target})=>{setEmail(target.value)}}
-        />
-        <Label for="exampleEmail">
-            Email
-        </Label>
-        </FormGroup>
-        {' '}
-        <FormGroup floating>
-        <Input
-            placeholder="Password"
-            type="Password"
-            onChange={({target})=>{setPassword(target.value)}}
-  />
-        <Label for="examplePassword">
-            Password
-        </Label>
-        </FormGroup>
-        {' '}
-        <div className='divbtn'>
-            <Button type='submit' className='btn' style={{background:'#080436',
-            color:'white',
-            borderColor:'#080436',
-            borderRadius:'35px',
-            width: '100px',
-            height:"50px",
-            fontSize: '20px',
-            fontFamily:"Roboto",
-            }}>Login</Button>
-        </div>
-    </Form>
+             <div className='divrightcenter'>
+                 <div className='iconemail'><AiOutlineMail/></div>
+                 <div className='iconpassword'><RiLockPasswordFill/></div>
+                 <h2 className='titleLog'>Login</h2>
+                 <Form inline className='formlogin' onSubmit={(e)=>{e.preventDefault();  SendLogueo(Email,Password)}} >
+                         <FormGroup floating >
+                         <Input            
+                             placeholder="Email"
+                             type="Email"
+                             onChange={({target})=>{setEmail(target.value)}}
+                         />
+                         <Label for="exampleEmail">
+                             Email
+                         </Label>
+                         </FormGroup>
+                         {' '}
+                         <FormGroup floating>
+                         <Input
+                             placeholder="Password"
+                             type="Password"
+                             onChange={({target})=>{setPassword(target.value)}}
+                   />
+                         <Label for="examplePassword">
+                             Password
+                         </Label>
+                         </FormGroup>
+                         {' '}
+                         <div className='divbtn'>
+                             <Button type='submit' className='btn' style={{background:'#080436',
+                             color:'white',
+                             borderColor:'#080436',
+                             borderRadius:'35px',
+                             width: '100px',
+                             height:"50px",
+                             fontSize: '20px',
+                             fontFamily:"Roboto",
+                             }}>Login</Button>
+                         </div>
+                     </Form>
+             </div>
    </div>
            
             </>
