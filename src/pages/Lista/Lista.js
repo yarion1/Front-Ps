96f323c8ef6bce60244373658d938504e91ddf03
Pedro Thomas <impedrothomas@outlook.com>
2022-04-28 19:47:03 -0300

    reposicionamento da paginacao

--- src/pages/Lista/Lista.js
+++ src/pages/Lista/Lista.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import Pagination  from '../Pagination/Pagination';
+import Pagination  from '../../components/Pagination/Pagination';
 
 import {Container,
         Input,
