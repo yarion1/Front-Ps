a2d63c107990f29446b191dc3886ac5ad52af644
marcosgilmario <marcosgilmario@gmail.com>
2022-05-30 11:54:04 -0300

    Fixing conflits

---
+++ src/components/UserProfileInfos/userprofileinfos.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import {Col,Label} from 'reactstrap';
+import styled, {
+    User_profile_container,
+    User_profile_namelabel,
+    User_profile_professionlabel,
+    User_profile_phonenumberlabel,
+    User_profile_photo,
+    User_profile_namefield,
+    User_profile_emailfield,
+    User_profile_phonenumberfield,
+    User_profile_descriptionfield,
+    User_profile_descriptionlabel,
+    Worker_profile_button, 
+
+  } from './styleduserprofileinfos.js';
+
+  class Worker_profile_infos extends Component {
+
+    render() {
+      return (
+         <User_profile_container>
+           <User_profile_photo></User_profile_photo>
+          <User_profile_namelabel>Nome</User_profile_namelabel>
+          <User_profile_namefield>João Pedro da Silva</User_profile_namefield>
+          <User_profile_professionlabel>e-mail</User_profile_professionlabel>
+          <User_profile_emailfield>joaopedro123@gmail.com</User_profile_emailfield>
+          <User_profile_phonenumberlabel>Telefone</User_profile_phonenumberlabel>
+          <User_profile_phonenumberfield>459321931293219</User_profile_phonenumberfield>
+          <User_profile_descriptionlabel>Descrição</User_profile_descriptionlabel>
+          <User_profile_descriptionfield></User_profile_descriptionfield>   
+        </User_profile_container>
+      )
+    }
+  }
+  export default Worker_profile_infos
