d9b72b1e3b5d1f9b256b1c53b0ab6e20d4232c1b
arthurffreis <arthurffreis@gmail.com>
2022-06-10 13:56:10 -0300

    Atualizacao da home page, componentes e pagina

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -8,6 +8,19 @@ import style,{
     Infopromo,
     Diagonalbox,
     Page,
+    PrincipalCard,
+    PrincipalCortediv,
+    PrincipalTitlecategoria,
+    PrincipalTextcategoria,
+    CardSignUp,
+    CardRent,
+    CardProfile,
+    cardfaixa,
+    Gambi,  Gamb, Gambi2, Gambi3,
+    CardButtons, CardButtons2, CardButtons3,
+    Textmid,
+    Sublinhado,
+    BotaoMais,
 
 } from './styledhomepage';
 import {
@@ -17,100 +30,124 @@ import {
 } from 'reactstrap';
 import {MdOutlineNavigateBefore, MdOutlineNavigateNext} from 'react-icons/md';
 import Navbar from '../../components/Navbar';
-
-
+import DemoCarousel from  '../../components/Carousel/Carousel';
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa'
+import { Carousel } from 'react-carousel-minimal';
+import { Link } from "react-router-dom";
 
 function Home () {
+  
   const cards = [
     {
       id: '1',
       title: 'Escavadeiras',
-      images: ['https://th.bing.com/th/id/OIP.OmC2cmBDXV-Dm_8BJnoRngHaEK?pid=ImgDet&rs=1'],
+      images: ['https://cdn.discordapp.com/attachments/931699948087574538/984492129525055518/pexels-anamul-rezwan-1078884.jpg'],
     },
 
     {
     id:'2',
-    title: 'Britadeiras',
-    images: ['https://www.inglesgratuito.com.br/wp-content/uploads/2015/08/FERRAMENTAS-EM-INGL%C3%8AS-02.jpg'],
+    title: 'Furadeiras',
+    images: ['https://cdn.discordapp.com/attachments/931699948087574538/984492220226875423/pexels-bidvine-1249611.jpg'],
     },
 
     {
     id:'3',
-    title: 'Tratores',
-    images: ['https://th.bing.com/th/id/R.a2b27751a88be66bcfd48579b3f1de55?rik=rc%2fWNJU5tCvwaw&pid=ImgRaw&r=0'],
+    title: 'Máquinas de costura',
+    images: ['https://cdn.discordapp.com/attachments/931699948087574538/984492283971911690/pexels-leticia-ribeiro-2249290.jpg'],
     },
 
     {
     id:'4',
     title: 'Ferramentas',
-    images: ['https://www.inglesgratuito.com.br/wp-content/uploads/2015/08/FERRAMENTAS-EM-INGL%C3%8AS-02.jpg'],
+    images: ['https://cdn.discordapp.com/attachments/931699948087574538/984492202392694814/pexels-pixabay-162553.jpg'],
     },
-  ]
-
+  ];
  return(
     <>
-      <Navbar/>
-      <Infopromo>
-        
-      </Infopromo>
-      <Page> 
-        <CardRowleft>
-          <MdOutlineNavigateBefore style={{fontSize:'40px', color:'#fff'}}/>
-        </CardRowleft>
-        <CardRowright>
-          <MdOutlineNavigateNext style={{fontSize:'40px', color:'#fff'}}/>
-        </CardRowright>
+      <Navbar/>   
+      <Page>
+        <Gambi3></Gambi3>
+        <DemoCarousel></DemoCarousel>
+
+        <CardSignUp>
+        <Gambi2></Gambi2>
+        <CardImg          
+          alt="Card image cap"
+          src= "https://cdn.discordapp.com/attachments/931699948087574538/984496160976810014/signup_1.png" 
+          style={{height: '90px'} , {width: '90px'}}
+          />
+          <Gambi></Gambi>
+          <Textcategoria>
+          Cadastre-se e tenha acesso a todos os recursos da plataforma.
+          </Textcategoria>
+          <Gamb></Gamb>
+          <CardButtons>
+            <Link to="/login" style={{ textDecoration: "none", fontSize: "20px", color:'#fff'}}>
+             Realizar Cadastro
+            </Link>
+          </CardButtons>
+        </CardSignUp>
+        <CardRent>
+        <Gambi2></Gambi2>
+        <CardImg          
+          alt="Card image cap"
+          src=   "https://cdn.discordapp.com/attachments/931699948087574538/984495692120719360/assembly_1.png"  
+          style={{height: '90px'} , {width: '90px'}}
+        />
+          <Gambi></Gambi>
+          <Textcategoria>
+          Alugue a máquina ideal para realizar suas atividades.
+          </Textcategoria>
+          <Gamb></Gamb>
+          <CardButtons2>
+            <Link to="/Rent" style={{ textDecoration: "none", fontSize: "20px", color:'#fff'}}>
+              Alugar máquina
+            </Link>
+          </CardButtons2>
+        </CardRent>
+        <CardProfile>
+        <Gambi2></Gambi2>
+        <CardImg            
+          alt="Card image cap"
+          src=  "https://cdn.discordapp.com/attachments/931699948087574538/984496035051237436/user_1.png" 
+          style={{height: '90px'} , {width: '90px'}}
+        />
+            <Gambi></Gambi>
+            <Textcategoria>
+            Atualize seu perfil para que todos possam conhecê-lo.
+            </Textcategoria>
+            <Gamb></Gamb>
+          <CardButtons3>
+            <Link to="/User_profile" style={{ textDecoration: "none", fontSize: "20px", color:'#fff'}}>
+              Acessar meu perfil
+            </Link>
+          </CardButtons3>
+        </CardProfile>
+        <Textmid>Escolha a máquina ideal para você!</Textmid>
+        <Sublinhado></Sublinhado>
         {cards.map((item, index) => (
         <Cardcategory inverse>
           <div 
            key={index}
           >
-          <CardImg
+           <CardImg
           alt="Card image cap"
           width="100%"
           src={item.images}
           style={{height: '250px'}}
           />
           <CardImgOverlay>
-            <Cortediv>
+            <Cortediv></Cortediv>
             <Textcategoria tag="h5">
                 {item.title}
               </Textcategoria>
-            </Cortediv>
           </CardImgOverlay>
           </div>
         </Cardcategory>
     ))}
-      <CardRowleft>
-          <MdOutlineNavigateBefore style={{fontSize:'40px', color:'#fff'}}/>
-        </CardRowleft>
-        <CardRowright>
-          <MdOutlineNavigateNext style={{fontSize:'40px', color:'#fff'}}/>
-        </CardRowright>
-        {cards.map((item, index) => (
-        <Cardcategory inverse>
-          <div 
-          key={index}
-          >
-          <CardImg
-          alt="Card image cap"
-          width="100%"
-          src={item.images}
-          style={{height: '250px'}}
-          />
-          <CardImgOverlay>
-            <Cortediv>
-            <Textcategoria tag="h5">
-                {item.title}
-              </Textcategoria>
-            </Cortediv>
-          </CardImgOverlay>
-            </div>
-        </Cardcategory>
-        ))}
       </Page>  
     </>
- );
+ ); 
 }
 
 export default Home;
